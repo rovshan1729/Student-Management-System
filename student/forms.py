177731fca175a888fd8830c214d27e9d8b497360
Rovshan <super777imba@gmail.com>
2024-01-29 23:42:25 +0500

    adding gitignore

--- student/forms.py
+++ student/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from .models import Student
 
-class StudentForm(forms.Form):
-    model = Student
-    fields = "__all__"
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+        widgets = {
+            'student_number':forms.NumberInput(attrs={'class':'form-control'}),
+            'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'field_of_study':forms.TextInput(attrs={'class':'form-control'}),
+            'gpa':forms.NumberInput(attrs={'class':'form-control'}),
+        }
